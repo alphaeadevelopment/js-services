e5273341be7308c1e75fe24c91e956855ac8adc4
Graham <gk@Grahams-iMac.local>
2018-01-15 08:19:33 +0000

    Fixed class

--- src/date/date-service.js
+++ src/date/date-service.js
@@ -1,18 +1,18 @@
 import moment from 'moment';
 
-const DateService = {
+class DateService {
   currentDate() {
     return moment();
-  },
+  }
   dateFromString(str, format) {
     return moment(str, format);
-  },
+  }
   dateToString(date, format) {
     return moment(date).format(format);
-  },
+  }
   daysSince(date, format) {
     return moment().diff(this.dateFromString(date, format), 'days');
-  },
-};
+  }
+}
 
 export default new DateService();
