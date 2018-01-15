aab8d36faaaf1f5255ef0e9aa2b909cec75e53ff
Graham <gk@Grahams-iMac.local>
2018-01-15 15:13:38 +0000

    always use stub service

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,10 @@ import ApiService from './api-service';
 import StubApiService from './stub/stub-api-service';
 
 let service = new ApiService();
-if (process.env.NODE_ENV !== 'production') {
-  service = new StubApiService(
-    service,
-    null,
-    { artificialDelay: 1000 },
-  );
-}
-const theService = service;
+service = new StubApiService(
+  service,
+  null,
+  { artificialDelay: 1000 },
+);
 
-export default theService;
+export default service;
