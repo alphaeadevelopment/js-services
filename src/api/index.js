6ee626fdca78f1b2ff6364f12c48a84d3282932e
Graham <gk@Grahams-iMac.local>
2018-01-21 19:25:59 +0000

    use const

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,11 @@
 import ApiService from './api-service';
 import StubApiService from './stub/stub-api-service';
 
-let service = new ApiService();
-service = new StubApiService(
+const service = new ApiService();
+const stubService = new StubApiService(
   service,
   null,
   { artificialDelay: 1000 },
 );
 
-export default service;
+export default stubService;
